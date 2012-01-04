6adc11f8e4c86fb7a6dd70b568fbd4ecf757cdbd
Chris Chapman <chris@pentandra.com>
2012-01-04 09:26:44 -0700

    Added search functionality and experimenting with layouts

--- xmonad.hs
+++ xmonad.hs
@@ -2,20 +2,29 @@
 -- xmonad config file.
 --
 
-
 import XMonad
-import XMonad.Hooks.SetWMName
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
 import XMonad.Hooks.ICCCMFocus
-import XMonad.Layout.Dishes
+import XMonad.Hooks.SetWMName
+import XMonad.Layout.Accordion
+import XMonad.Layout.Combo
+import XMonad.Layout.DragPane
+import XMonad.Layout.Gaps
+import XMonad.Layout.Named
 import XMonad.Layout.NoBorders
 import XMonad.Layout.ResizableTile
-import XMonad.Layout.Gaps
+import XMonad.Layout.StackTile
+import XMonad.Layout.WindowNavigation
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
+
+import qualified Data.Map              as M
+import qualified XMonad.Prompt         as P
+import qualified XMonad.Actions.Submap as SM
+import qualified XMonad.Actions.Search as S
 
 -- Use windows key for mod
 myModMask = mod4Mask
@@ -26,12 +35,8 @@ myTerminal = "urxvtc"
 -- Fixes JAVA Swing applications with taking and losing focus.
 myLogHook = takeTopFocus
 
--- Fixes JAVA Swing applications even more
-myStartupHook = do
-    setWMName "LG3D"
-    spawn "urxvtc -e mutt"
-    spawn "urxvtc -e htop"
-    spawn "firefox"
+-- Convinces JAVA Swing applications that I am using Project Looking Glass
+myStartupHook = setWMName "LG3D"
 
 -- my workspaces
 ws_network        = "1:net"
@@ -51,7 +56,10 @@ myManageHook = composeAll
     ]
 
 -- layout
-myLayout = ResizableTall 1 (3/100) (1/2) [] ||| Dishes 2 (1/6) ||| noBorders Full
+myLayout = tiled ||| noBorders Full ||| wmii
+  where
+    tiled = named "Default" (ResizableTall 1 (1/100) (1/2) [])
+    wmii = windowNavigation (named "Wmii" (combineTwo (dragPane Vertical 0.01 0.5) (Accordion) (Accordion)))
 
 main = do
   xmproc <- spawnPipe "/usr/bin/xmobar /home/chris/.xmonad/xmobarrc"
@@ -72,8 +80,30 @@ main = do
     }
     `additionalKeys`
     [ ((myModMask .|. shiftMask,  xK_z),      spawn "xscreensaver-command -lock")
-    , ((controlMask,              xK_Print),  spawn "sleep 0.2; scrot -s")
+    , ((myModMask,                xK_Print),  spawn "scrot -b -s")
     , ((0,                        xK_Print),  spawn "scrot")
+
+    -- Keys for ResizableTall layout
     , ((myModMask,                xK_a),      sendMessage MirrorShrink)
     , ((myModMask,                xK_z),      sendMessage MirrorExpand)
+
+    -- Keys for WindowNavigation
+    , ((myModMask,                xK_Right),  sendMessage $ Go R)
+    , ((myModMask,                xK_Left ),  sendMessage $ Go L)
+    , ((myModMask,                xK_Up   ),  sendMessage $ Go U)
+    , ((myModMask,                xK_Down ),  sendMessage $ Go D)
+    , ((myModMask .|. shiftMask,  xK_Right),  sendMessage $ Move R)
+    , ((myModMask .|. shiftMask,  xK_Left ),  sendMessage $ Move L)
+    , ((myModMask .|. shiftMask,  xK_Up   ),  sendMessage $ Move U)
+    , ((myModMask .|. shiftMask,  xK_Down ),  sendMessage $ Move D)
+
+    -- keybindings for searching
+    , ((myModMask,                xK_s),      SM.submap $ searchEngineMap $ S.promptSearch P.defaultXPConfig)
+    , ((myModMask .|. shiftMask,  xK_s),      SM.submap $ searchEngineMap $ S.selectSearch)
+    ]
+
+searchEngineMap method = M.fromList $
+    [ ((0, xK_g), method S.google)
+    , ((0, xK_w), method S.wikipedia)
+    , ((0, xK_d), method S.dictionary)
     ]
